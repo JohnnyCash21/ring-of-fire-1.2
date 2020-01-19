5e1205b504abeabc0c2bb7fd522e4ca46102496a
JohnnyCash21 <59767971+JohnnyCash21@users.noreply.github.com>
2020-01-19 17:32:41 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -104,7 +104,7 @@ Client.on('message', (message)=>{
     if(message.content.startsWith(prefix + "image")) {
         number = 43;
         imageNumber = Math.floor (Math.random() * (number - 1 + 1)) + 1;
-        message.channel.send ( {files: ["./images/" + imageNumber + ".PNG"]} )
+        message.channel.send ( {files: ["./1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43" + imageNumber + ".PNG"]} )
     }
 
 
