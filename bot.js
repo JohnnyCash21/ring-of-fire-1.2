d705dbcf6f0df400f8ecb4185347454823ce0cb3
JohnnyCash21 <59767971+JohnnyCash21@users.noreply.github.com>
2020-01-19 18:56:31 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -101,61 +101,14 @@ Client.on('message', (message)=>{
 
 
 
-const photonum = [
-        "./1.PNG/",
-        "./2.PNG/",
-        "./3.PNG/",
-        "./4.PNG/",
-        "./5.PNG/",
-        "./6.PNG/",
-        "./7.PNG/",
-        "./8.PNG/",
-        "./9.PNG/",
-        "./10.PNG/",
-        "./11.PNG/",
-        "./12.PNG/",
-        "./13.PNG/",
-        "./14.PNG/",
-        "./15.PNG/",
-        "./16.PNG/",
-        "./17.PNG/",
-        "./18.PNG/",
-        "./19.PNG/",
-        "./20.PNG/",
-        "./21.PNG/",
-        "./22.PNG/",
-        "./23.PNG/",
-        "./24.PNG/",
-        "./25.PNG/",
-        "./26.PNG/",
-        "./27.PNG/",
-        "./28.PNG/",
-        "./29.PNG/",
-        "./30.PNG/",
-        "./31.PNG/",
-        "./32.PNG/",
-        "./33.PNG/",
-        "./34.PNG/",
-        "./35.PNG/",
-        "./36.PNG/",
-        "./37.PNG/",
-        "./38.PNG/",
-        "./39.PNG/",
-        "./40.PNG/",
-        "./41.PNG/",
-        "./42.PNG/",
-        "./43.PNG/"
 
 
-    ];
-
-    if(message.content.startsWith(prefix + "image")){
+if(message.content.startsWith(prefix + "image")){
         photonumber = 43;
 
         const randomPhoto = Math.floor(Math.random() * (photonumber - 1 + 1)) + 1;
-    message.channel.send( {files: ["photonum"]} [randomPhoto]);
+    message.channel.send ( {files: ["./1.PNG", "./2.PNG/"]}[randomPhoto]);
     }
-
 
     const response  = [
         "It is certain.",
