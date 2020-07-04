0315231aca15e549a9047d37c3a10e468dd15976
JohnnyCash21 <59767971+JohnnyCash21@users.noreply.github.com>
2020-07-04 16:28:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,6 +17,9 @@ const { RichEmbed } = require("discord.js");
 const { stripIndents } = require("common-tags");
 var version = '1.2';
 var servers = {};
+let xp = require("./xp.json");
+const fs = require("fs");
+
 
 
 
@@ -725,6 +728,54 @@ if(message.content.startsWith(prefix + "photoshop")){
         .setColor(0xF1C40F)
         message.channel.send(adminEmbed)
     }
+    
+    
+    
+    
+    let xpAdd = Math.floor(Math.random() * 7) + 8;
+    
+
+    if(!xp[message.author.id]){
+        xp[message.author.id] = {
+            xp: 0,
+            level: 1
+        };
+    }
+
+    let curxp = xp[message.author.id].xp;
+    let curlevel = xp[message.author.id].level;
+    let nextLevel = xp[message.author.id].level * 300;
+    let nextLevelxp = curlevel * 300;
+    let difference = nextLevelxp - curxp;
+    xp[message.author.id].xp = curxp + xpAdd;
+
+    if(nextLevel <= xp[message.author.id].xp){
+        xp[message.author.id].level = curlevel + 1;
+        
+        let lvlup = new Discord.RichEmbed()
+        .setTitle('Level Up!')
+        .setColor('#7a34eb')
+        .addField("New Level", curlevel + 1)
+        .addField(`${message.author.username}`, "has leveled up!")
+        message.channel.send(lvlup)
+        
+
+    }
+
+    if(message.content.startsWith(prefix + "level")){
+        let lvlEmbed = new Discord.RichEmbed()
+        .setAuthor(message.author.username)
+        .setColor('#3614a6')
+        .addField('Level:', curlevel, true)
+        .addField('XP:', curxp, true)
+        .setFooter(`${difference} XP till next level!`, message.author.displayAvatarURL);
+        message.reply(lvlEmbed);
+    }
+    
+
+    fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
+        if(err) console.log(err)
+    });
 
         
 
