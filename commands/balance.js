a17323f0645d7fd2df83f8d5b61d20ba4344c8d0
JohnnyCash21 <59767971+JohnnyCash21@users.noreply.github.com>
2020-07-05 22:05:59 +0100

    Create balance.js

---
+++ commands/balance.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const money = require("../money.json");
+
+
+module.exports.run = async (Client, message, args) => {
+
+    if(!args[0]){
+        var user = message.author;
+    } else {
+        var user = message.mentions.users.first() || Client.users.get(args[0]);
+    }
+
+
+    if(!money[user.id]) {
+        money[user.id] = {
+            name: Client.users.get(user.id).tag,
+            money: 0
+        }
+        fs.writeFile("./money.json", JSON.stringify(money), (err) => {
+            if(err) console.log(err)
+        });
+    }
+
+
+    return message.channel.send(`${Client.users.get(user.id).username} has ${money[user.id].money} cash!`)
+    
+
+}
+
+module.exports.help = {
+    name: "balance",
+    aliases: ["bal", "money"]
+}
