7ad3403a70acedeb46762796b87aebb24712235a
JohnnyCash21 <59767971+JohnnyCash21@users.noreply.github.com>
2020-08-23 15:54:34 +0100

    Create invite-notifications.js

---
+++ commands/invite-notifications.js
@@ -0,0 +1,57 @@
+const { GuildAuditLogs } = require("discord.js")
+
+module.exports = Client => {
+    const invites = {}
+
+    const getInviteCounts = async (guild) => {
+        return await new Promise(resolve => {
+            guild.fetchInvites().then(invites => {
+                const inviteCounter = {}
+
+                invites.forEach(invite => {
+                    const { uses, inviter } = invite
+                    const { username, discriminator } = inviter
+
+                    const name = `${username}#${discriminator}`
+
+                    inviteCounter[name] = (inviteCounter[name] || 0) + uses
+
+
+                });
+
+                resolve(inviteCounter)
+            })
+        })
+
+    }
+
+    Client.guilds.forEach(async (guild) => {
+        invites[guild.id] = await getInviteCounts(guild);
+
+    })
+
+    Client.on('guildMemberAdd', async member => {
+        const { guild } = member
+
+        const invitesBefore = invites[guild.id]
+        const invitesAfter = await getInviteCounts(guild)
+
+        console.log('BEFORE:', invitesBefore);
+        console.log('AFTER:', invitesAfter)
+
+        for(const inviter in invitesAfter){
+            if(invitesBefore[inviter] === invitesAfter[inviter] - 1){
+                const channel = guild.channels.find(channel => channel.name === "general");
+                const count = invitesAfter[inviter]
+                channel.send(`Please welcome ${member} to the Discord! Invited by ${inviter} (${count} total invites)`)
+                console.log("EE")
+                return
+            }
+        }
+    })
+}
+
+module.exports.help = {
+    name: "",
+    aliases: []
+}
