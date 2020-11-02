8a028324817d7660390de37026090387bcef19a2
JohnnyCash21 <59767971+JohnnyCash21@users.noreply.github.com>
2020-11-02 20:49:44 +0000

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,34 +1,42 @@
-const { RichEmbed } = require('discord.js')
+const { MessageEmbed } = require('discord.js')
 const { stripIndents } = require('common-tags')
 
 module.exports.run = async (Client, message, args) => {
     if(message.deletable) message.delete();
 
-    let rMember = message.mentions.members.first() || message.guild.members.get(args[0]);
+    let rMember = message.mentions.members.first() || message.guild.members.fetch(args[0]);
+    
 
-    if(!rMember) return message.reply("Couldn't find the person you are looking for. \nRemember to use the command as so: `!report (user) (reason)`").then(m => m.delete(5000));
+    if(!args[0]) return message.reply("Couldn't find the person you are looking for. \nRemember to use the command as so: `!report (user) (reason)`").then(m => m.delete({ timeout: 5000 }));
 
-    if(rMember.hasPermission("KICK_MEMBERS") || rMember.user.bot){
-        return message.reply("I wasn't able to report this user.").then(m => m.delete(5000))
+    try{
+        if(rMember.permissions.has("KICK_MEMBERS") || rMember.user.bot){
+            return message.reply("I wasn't able to report this user.").then(m => m.delete({ timeout: 5000 }))
+        }
     }
+    catch(err){
+        return message.channel.send("An error occurred! Are you trying to do something you're not supposed to?")
+
+    }
+    
 
     if(!args[1]){
-        return message.channel.send("Please provide a reason for me to report this user! \nRemember to use the command as so: `!report (user) (reason)`").then(m => m.delete(5000));
+        return message.channel.send("Please provide a reason for me to report this user! \nRemember to use the command as so: `!report (user) (reason)`").then(m => m.delete({ timeout: 5000 }));
     }
 
-    const channel = message.guild.channels.find(channel => channel.name === "reports");
+    const channel = message.guild.channels.cache.find(channel => channel.name === "reports");
 
-    if(!channel) return message.channel.send("Please create a `reports` channel before using this command").then(m => m.delete(5000));
+    if(!channel) return message.channel.send("Please create a `reports` channel before using this command").then(m => m.delete({ timeout: 5000 }));
 
-    const embed = new RichEmbed()
+    const embed = new MessageEmbed()
         .setColor("#d1271b")
         .setTimestamp()
-        .setFooter(message.guild.name, message.guild.iconURL)
-        .setAuthor("Reported Member", rMember.user.displayAvatarURL)
+        .setFooter(message.guild.name, message.guild.iconURL())
+        .setAuthor("Reported Member", rMember.user.displayAvatarURL())
         .setDescription(stripIndents`**- Member:** ${rMember} (${rMember.id})
-        **- Reported by:** ${message.member} (${message.member.id})
-        **- Reported in:** ${message.channel}
-        **- Reason:** ${args.slice(1).join(" ")}`);
+        \n**- Reported by:** ${message.member} (${message.member.id})
+        \n**- Reported in:** ${message.channel}
+        \n**- Reason:** ${args.slice(1).join(" ")}`);
     
     message.channel.send("Successfuly sent report!")
     return channel.send(embed)
