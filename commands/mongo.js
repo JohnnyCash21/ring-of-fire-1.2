e4d9f6e4a365e6cbb579fe12874a7d9da774f2b6
JohnnyCash21 <59767971+JohnnyCash21@users.noreply.github.com>
2021-01-05 12:03:04 +0000

    Update mongo.js

--- commands/mongo.js
+++ commands/mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoPath = process.env.MONGO_URL || "mongodb://localhost/discordBot_db"
+const mongoPath = process.env.MONGO_URL || "mongodb://localhost:27017/discordBot"
 
 module.exports = async () => {
     await mongoose.connect(mongoPath, {
