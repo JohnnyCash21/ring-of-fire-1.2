427123535c63efd409050d6156b790567274ad04
JohnnyCash21 <59767971+JohnnyCash21@users.noreply.github.com>
2021-01-08 12:51:49 +0000

    Update rob.js

--- commands/rob.js
+++ commands/rob.js
@@ -1,5 +1,6 @@
-const money = require("../money.json");
-const fs = require("fs");
+const mongo = require("./mongo")
+
+const Data = require("../schemas/data")
 
 module.exports.run = async (Client, message, args) => {
     
@@ -8,42 +9,55 @@ module.exports.run = async (Client, message, args) => {
     let user = message.mentions.members.first() || Client.users.cache.get(args[0]);
     if(!user) return message.reply("Sorry, couldn't find that user.");
 
-    if(!args[1]) return message.reply("Please specify the amount you want to rob.");
+    if(user.id === message.author.id) return message.reply("You can't rob yourself!")
 
-    
-    if(!money[message.author.id]) return message.reply("Sorry, you don't have any cash.");
-    if(money[message.author.id].money < 750) return message.reply("You need 750 or more cash to rob");
+    await mongo().then(async (mongoose) => {
+        try {
+            await Data.findOne({
+                userId: message.author.id
+            }, (err, authorData) => {
+                if(err) console.log(err);
+                if(!authorData || authorData.money < 750){
+                    return message.reply("You need atleast 750 cash to rob!")
+                } else {
+                    Data.findOne({
+                        userId: user.id
+                    }, (err, userData) => {
+                        if(err) console.log(err);
+                        if(!args[1]) return message.reply("Please specify the amount you want to rob.");
 
-    if(parseInt(args[1]) < 1) return message.reply("You cannot rob less than 1 cash!");
-    if(parseInt(args[1]) > 7000) return message.reply("You cannot rob more than 7000 cash!");
-    if(!parseInt(args[1])) return message.reply("You can only rob in integers.");
+                        if(parseInt(args[1]) < 1) return message.reply("You cannot rob less than 1 cash!");
+                        if(parseInt(args[1]) > 7000) return message.reply("You cannot rob more than 7000 cash!");
+                        if(!parseInt(args[1])) return message.reply("You can only rob in integers.");
 
-    if(!money[user.id]) {
-        
-        return message.channel.send("This person has no money!")
-    } else {
-        if(money[user.id].money < args[1]) return message.reply("This person doesn't have that much cash");
+                        if(!userData || userData.money < parseInt(args[1])){
+                            return message.reply("This person doesn't have that much money!")
 
-        const chance = Math.floor(Math.random() * 2);
-        
-        if(chance == 0){
-            money[user.id].money -= parseInt(args[1]);
-            money[message.author.id].money += parseInt(args[1]);
-        }else{
-            money[user.id].money += 500;
-            money[message.author.id].money -= 500;
-            return message.channel.send("You got caught! You had to pay a fine of 500 cash to the other user.")
+                        }else{
+                            const chance = Math.floor(Math.random() * 2);
+                            if(chance == 0){
+                                userData.money -= parseInt(args[1]);
+                                authorData.money += parseInt(args[1]);
+                                userData.save().catch(err => console.log(err));
+                                authorData.save().catch(err => console.log(err));
+                            }else{
+                                userData.money += 500;
+                                authorData.money -= 500;
+                                userData.save().catch(err => console.log(err));
+                                authorData.save().catch(err => console.log(err));
+                                return message.channel.send("You got caught! You had to pay a fine of 500 cash to the other user.")
 
+                            }
+                            return message.channel.send(`${message.author.username} robbed ${args[1]} cash from ${Client.users.cache.get(user.id)}!`);
+                        }
+                    })
+                }
+            })
+
+        } finally {
+            //mongoose.connection.close()
         }
-        fs.writeFile("./money.json", JSON.stringify(money), (err) => {
-            if(err) console.log(err);
-        });
-
-        
-    }
-
-    return message.channel.send(`${message.author.username} robbed ${args[1]} cash from ${Client.users.cache.get(user.id)}!`);
-
+    })
 
 
 }
