a3738b01b2558d0361936d690193bcb381405223
JohnnyCash21 <59767971+JohnnyCash21@users.noreply.github.com>
2021-01-08 12:50:13 +0000

    Update gamble.js

--- commands/gamble.js
+++ commands/gamble.js
@@ -1,19 +1,9 @@
 const Discord = require("discord.js");
-const ms = require("parse-ms");
 const mongo = require("./mongo")
 
 const Data = require("../schemas/data")
 
 module.exports.run = async (Client, message, args) => {
-
-    let timeout = 86400000;    //86400000
-    let reward = 500;
-
-    let dailyEmbed = new Discord.MessageEmbed();
-
-    if(message.author.bot) return;
-        
-    dailyEmbed.setTitle('Daily Reward');
 
     await mongo().then(async (mongoose) => {
         try {
@@ -26,42 +16,82 @@ module.exports.run = async (Client, message, args) => {
                         name: message.author.username,
                         userId: message.author.id,
                         lb: "all",
-                        money: reward,
-                        daily: Date.now(),
+                        money: 0,
+                        daily: 0,
                     })
                     newData.save().catch(err => console.log(err));
-                    message.channel.send(`${message.author.username} has ${reward} cash.`)
-                } else {
-                    if(timeout - (Date.now() - data.daily) > 0){
-                        let time = ms(timeout - (Date.now() - data.daily));
+                    message.reply("You have no cash to gamble with!")
+                }else{
+                    let gambleEmbed = new Discord.MessageEmbed()
 
-                        dailyEmbed.setColor("#e63127");
-                        dailyEmbed.setDescription(`**You already collected your daily reward. Come back later!**`);
-                        dailyEmbed.addField(`Collect again in`, `**${time.hours}h ${time.minutes}m ${time.seconds}s**`);
-                        return message.reply(dailyEmbed);
+                    if(message.author.bot) return;
 
-                    } else{
-                        data.money += reward;
-                        data.daily = Date.now();
-                        data.save().catch(err => console.log(err));
+                    if(data.money <= 0) return message.reply("You don't have any cash!");
 
-                        dailyEmbed.setDescription(`You collected your daily reward of ${reward} cash! Current balance is ${data.money} cash.`);
-                        dailyEmbed.setColor("#27e65a");
-                        return message.reply(dailyEmbed);
+                    if(!args[0]) return message.reply("Please specify the amount of cash you want to gamble.");
 
+                    try {
+                        var bet = parseFloat(args[0]);
+                    } catch {
+                        return message.reply("You can only enter whole integers.");
                     }
+
+                    if(parseFloat(args[0]) < 1) return message.reply("You cannot gamble less than 1 cash!");
+
+                    if(bet != Math.floor(bet)) return message.reply("You can only enter whole integers.");
+
+                    if(data.money < bet) return message.reply("You don't have that much cash to gamble with!");
+
+                    let chances = ["Win", "Lose"];
+                    let pick = chances[Math.floor(Math.random() * chances.length)];
+
+                    if(pick == "Lose") {
+                        data.money -= bet;
+                        data.save().catch(err => console.log(err));
+                        gambleEmbed.setTitle(`${message.author.username}'s Gambling Game`);
+                        gambleEmbed.addField("YOU LOST!", `New Balance: ${data.money} cash.`);
+                        gambleEmbed.setColor("#e63127");
+                        return message.reply(gambleEmbed);
+                    } else {
+                        jackpot = 1000
+                        const randomIndexJackpot = Math.floor(Math.random() * (jackpot - 1 + 1)) + 1;
+                        console.log(randomIndexJackpot)
+                        if(randomIndexJackpot == 439){
+                            data.money += 1000000;
+                            data.save().catch(err => console.log(err));
+                            
+                            gambleEmbed.setTitle(`${message.author.username}'s Gambling Game`);
+                            gambleEmbed.addField("JACKPOT!", `New Balance: ${data.money} cash.`);
+                            gambleEmbed.setColor("#ffef12");
+                            return message.reply(gambleEmbed);
+                        } else {
+                            data.money += bet;
+                            data.save().catch(err => console.log(err));
+                            gambleEmbed.setTitle(`${message.author.username}'s Gambling Game`);
+                            gambleEmbed.addField("YOU WIN!", `New Balance: ${data.money} cash.`);
+                            gambleEmbed.setColor("#27e65a");
+                            return message.reply(gambleEmbed);
+
+                        }
+                        
+                    }
+
                 }
+                
             })
 
         } finally {
+            console.log("Closing connecttion")
             mongoose.connection.close()
         }
     })
 
     
+
+
 }
 
 module.exports.help = {
-    name: "daily",
+    name: "gamble",
     aliases: []
 }
