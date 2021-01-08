62eafd20aa8231077b75be4554d8fb75e436a93a
JohnnyCash21 <59767971+JohnnyCash21@users.noreply.github.com>
2021-01-08 12:51:19 +0000

    Update pay.js

--- commands/pay.js
+++ commands/pay.js
@@ -1,5 +1,6 @@
-const money = require("../money.json");
-const fs = require("fs");
+const mongo = require("./mongo")
+
+const Data = require("../schemas/data")
 
 module.exports.run = async (Client, message, args) => {
     
@@ -8,41 +9,54 @@ module.exports.run = async (Client, message, args) => {
     let user = message.mentions.members.first() || Client.users.cache.get(args[0]);
     if(!user) return message.reply("Sorry, couldn't find that user.");
 
-    if(!args[1]) return message.reply("Please specify the amount you want to pay.");
+    if(user.id === message.author.id) return message.reply("You can't pay yourself!")
 
-    
-    if(!money[message.author.id]) return message.reply("Sorry, you don't have any money.");
+    await mongo().then(async (mongoose) => {
+        try {
+            await Data.findOne({
+                userId: message.author.id
+            }, (err, authorData) => {
+                if(err) console.log(err);
+                if(!authorData){
+                    message.reply("You have no cash to pay with!")
+                } else {
+                   Data.findOne({
+                        userId: user.id
+                    }, (err, userData) => {
+                        if(err) console.log(err);
+                        if(!args[1]) return message.reply("Please specify the amount you want to pay.");
 
-    if(parseInt(args[1]) > money[message.author.id].money) return message.reply("You do not have enough cash!");
-    if(parseInt(args[1]) < 1) return message.reply("You cannot pay less than 1 cash!");
-    if(!parseInt(args[1])) return message.reply("You can only pay in integers.");
+                        if(parseInt(args[1]) > authorData.money) return message.reply("You do not have enough cash!");
+                        if(parseInt(args[1]) < 1) return message.reply("You cannot pay less than 1 cash!");
+                        if(!parseInt(args[1])) return message.reply("You can only pay in integers.");
+                        if(!userData){
+                            const newData = new Data({
+                                name: Client.users.cache.get(user.id).username,
+                                userId: user.id,
+                                lb: "all",
+                                money: parseInt(args[1]),
+                                daily: 0,
+                            })
+                            authorData.money -= parseInt(args[1])
+                            newData.save().catch(err => console.log(err));
+                            authorData.save().catch(err => console.log(err));
 
-    if(!money[user.id]) {
-        
-        money[user.id] = {
-            name: Client.users.cache.get(user.id).tag,
-            money: parseInt(args[1])
+                        }else{
+                            userData.money += parseInt(args[1])
+                            authorData.money -= parseInt(args[1])
+                            userData.save().catch(err => console.log(err));
+                            authorData.save().catch(err => console.log(err));
+                        }
+
+                        return message.channel.send(`${message.author.username} payed ${args[1]} cash to ${Client.users.cache.get(user.id)}!`);
+                    })
+                }
+            })
+
+        } finally {
+            //mongoose.connection.close()
         }
-
-        money[message.author.id].money -= parseInt(args[1]);
-
-        fs.writeFile("./money.json", JSON.stringify(money), (err) => {
-            if(err) console.log(err);
-        });
-    } else {
-
-        money[user.id].money += parseInt(args[1]);
-
-        money[message.author.id].money -= parseInt(args[1]);
-
-        fs.writeFile("./money.json", JSON.stringify(money), (err) => {
-            if(err) console.log(err);
-        });
-    }
-
-    return message.channel.send(`${message.author.username} payed ${args[1]} cash to ${Client.users.cache.get(user.id)}!`);
-
-
+    })
 
 }
 
